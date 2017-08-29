ea1680f665cdb8f41463da3cf9878562e3bba272
brodan <christopher.hranj@gmail.com>
2017-08-29 02:13:49 -0400

    add notifications section to travis config, fix greet to use template literal, finish dummy tests

--- test/test_greet.js
+++ test/test_greet.js
@@ -2,9 +2,14 @@ const greet = require ('../index.js')
 const assert = require('assert');
 
 describe('greet', function() {
-  describe('when given name', function() {
-    it('should return "Hello, World" when not given a name', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+  describe('when given the name "Brodan"', function() {
+    it('should return "Hello, Brodan!"', function() {
+      assert.equal("Hello, Brodan!", greet.greet("Brodan"));
+    });
+  });
+  describe('when given no name', function() {
+    it('should return "Hello, World!"', function() {
+      assert.equal("Hello, World!", greet.greet());
     });
   });
 });
