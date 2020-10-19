4c986614f3edd0be5c76f478f296957ea274f18d
engg-ankit2212 <72604990+engg-ankit2212@users.noreply.github.com>
2020-10-19 12:23:23 +0530

    Update test_greet.js

--- test/test_greet.js
+++ test/test_greet.js
@@ -2,9 +2,9 @@ const greet = require ('../index.js')
 const assert = require('assert');
 
 describe('greet', function() {
-  describe('when given the name "Brodan"', function() {
-    it('should return "Hello, Brodan!"', function() {
-      assert.equal("Hello, Brodan!", greet.greet("Brodan"));
+  describe('when given the name "World"', function() {
+    it('should return "Hello, World!"', function() {
+      assert.equal("Hello, Brodan!", greet.greet("World"));
     });
   });
 
@@ -14,9 +14,9 @@ describe('greet', function() {
     });
   });
 
-  describe('when designed to fail', function() {
-    it('should fail', function() {
-      assert.fail(null, null, 'this should fail');
-    });
-  });
+//   describe('when designed to fail', function() {
+//     it('should fail', function() {
+//       assert.fail(null, null, 'this should fail');
+//     });
+//   });
 });
